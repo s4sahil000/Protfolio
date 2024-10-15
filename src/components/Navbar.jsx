5ed38ab8ea93b719412318d8bcbdeef93af89f82
talaDeepak <panghaldee@gmail.com>
2024-10-15 17:57:52 +0530

    Done with coding

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+
+import { logo } from "../assets/images";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-14 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">Sahil</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
